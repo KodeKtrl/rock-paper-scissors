adca462e3bfeba23818aea4a4630a0123511d4c6
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-03-21 18:53:39 +0530

    player and  computer choice added

--- java.js
+++ java.js
@@ -1,1 +1,41 @@
 console.log("test connection")
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+function computerChoice(choice){
+    randomNumber = Math.floor(Math.random() * 3)
+    if (randomNumber == 0){
+        choice = "rock"
+    }
+    else if (randomNumber == 1){
+        choice = "paper"
+    }
+    else{
+        choice = "scissor"
+    }
+    return choice
+}
+
+function playerChoice(choice){
+    choice = prompt("1. Rock, 2. Paper or 3. Scissors?")
+    if (choice == 1){
+        choice = "rock"
+    }
+    else if (choice == 2){
+        choice = "paper"
+    }
+    else if(choice == 3){
+        choice = "scissors"
+    }
+    else{
+        choice = "unknown value"
+    }
+    return choice
+    
+}
+
+console.log(playerChoice())
+console.log(computerChoice())
