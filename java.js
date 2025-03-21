5d49977a3578f65e9025b1f97f2ce5fc99adf308
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-03-21 19:45:46 +0530

    score added, rounds added

--- java.js
+++ java.js
@@ -5,31 +5,55 @@ let computerScore = 0;
 
 
 
-function getComputerChoice(choice){
+function getComputerChoice(computerChoice){
     randomNumber = Math.floor(Math.random() * 3)
     if (randomNumber == 0){
-        choice = "rock"
+        computerChoice = "rock"
     }
     else if (randomNumber == 1){
-        choice = "paper"
+        computerChoice = "paper"
     }
     else{
-        choice = "scissor"
+        computerChoice = "scissors"
     }
-    return choice
+    return computerChoice
 }
 
 function getPlayerChoice(choice){
     choice = prompt("Rock, Paper or Scissors?:")
+    return choice.toString()
 }
 
-function playRound(humanChoice, ComputerChoice){
-    console.log("You lose! Paper is king.")
-    computerScore += 1;
+
+function playRound(){
+    const humanChoice = getPlayerChoice().toLowerCase()
+    const computerChoice = getComputerChoice()
+   
+
+    if (humanChoice == "rock" && computerChoice == "paper"){
+        console.log("Computer wins!");
+        computerScore += 1;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors"){
+        console.log("Computer wins!");
+        computerScore += 1;
+    }
+    else if(humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("COmputer wins!");
+        computerScore += 1;
+    }
+    else if (humanChoice == computerChoice){
+        console.log("it is a tie!")
+    }
+    else{
+        console.log("You win!");
+        playerScore += 1;
+    }
 }
 
-for (i = 0; i <5; i++){
-    playRound()
+
+for (i =0; i < 5; i++){
+    playRound();
+    console.log(i)
 }
 
-console.log(computerScore)
