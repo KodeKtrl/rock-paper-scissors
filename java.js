22eda9ce208c9fde93d737d78bd6de4aad286e31
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-03-24 11:20:13 +0530

    ui added

--- java.js
+++ java.js
@@ -19,14 +19,10 @@ function getComputerChoice(computerChoice){
     return computerChoice
 }
 
-function getPlayerChoice(choice){
-    choice = prompt("Rock, Paper or Scissors?:")
-    return choice.toString()
-}
-
+let getPlayerChoice = null;
 
 function playRound(){
-    const humanChoice = getPlayerChoice().toLowerCase()
+    const humanChoice = getPlayerChoice
     const computerChoice = getComputerChoice()
    
 
@@ -34,14 +30,39 @@ function playRound(){
         humanChoice == "paper" && computerChoice == "scissors"||
         humanChoice == "scissors" && computerChoice == "rock"
     ){
-        console.log(`You chose ${humanChoice}, computer chose${computerChoice}. Computer wins.`);
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. Computer wins.`);
         computerScore += 1;
     }
     else if (humanChoice == computerChoice){
         console.log("it is a tie!")
+        console.log(`${humanChoice} and ${computerChoice}`)
     }
     else{
         console.log(`You chose ${humanChoice}, computer chose${computerChoice}. You win.`);
         playerScore += 1;
     }
 }
+
+const rock = document.querySelector('#rock')
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+const choices = document.querySelector('#choices')
+
+choices.addEventListener('click', (event) =>{
+    let target = event.target;
+
+    switch(target.id){
+        case 'rock':
+            getPlayerChoice = 'rock'
+            playRound()
+            break;
+        case 'paper':
+            getPlayerChoice = 'paper'
+            playRound()
+            break;
+        case 'scissors':
+            getPlayerChoice = 'scissors'
+            playRound()
+            break;
+    }
+});
